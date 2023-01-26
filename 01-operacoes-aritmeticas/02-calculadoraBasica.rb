810daedc01241a87eee62eaf531aa8f187190cae
Fabio Rigaud <fabio_rigaud@hotmail.com>
2023-01-26 23:52:30 +0000

    exercicio 2

---
+++ 01-operacoes-aritmeticas/02-calculadoraBasica.rb
@@ -0,0 +1,11 @@
+print "Informe o primeiro número inteiro: "
+numb1 = gets.chomp.to_i
+print "Informe o segundo número inteiro: "
+numb2 = gets.chomp.to_i
+
+puts "\nSoma = #{numb1 + numb2}"
+puts "Subtração = #{numb1 - numb2}"
+puts "Multiplicação = #{numb1 * numb2}"
+puts "Divisão = #{numb1.to_f / numb2.to_f}"
+puts "Resto = #{numb1 % numb2}"
+puts "Potenciação = #{numb1 ^ numb2}"
