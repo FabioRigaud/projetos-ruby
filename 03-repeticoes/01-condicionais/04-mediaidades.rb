a9bdc172fbd60748765afc13e3f64dc49ac17ca7
Fabio Rigaud <fabio_rigaud@hotmail.com>
2023-02-13 16:41:32 +0000

    media de idades correto

--- 03-repeticoes/01-condicionais/04-mediaidades.rb
+++ 03-repeticoes/01-condicionais/04-mediaidades.rb
@@ -3,11 +3,16 @@ years = gets.chomp.to_f
 sum = 0
 cont = 0
 
-while years > 0 if
-    sum += years
-    cont += 1
+while if years > 0
+        sum += years
+        cont += 1
+        years = gets.chomp.to_f
+    end
 end
 
+if sum > 0
 avg = sum/cont
-
 puts "MEDIA = #{avg}"
+else
+puts 'IMPOSSÍVEL CALCULAR'
+end
